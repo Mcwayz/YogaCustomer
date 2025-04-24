597bc8af02d8650853b1792444858141b734465d
mataa019 <johnnymataa019@gmail.com>
2025-04-24 10:40:24 +0200

    Setup Signup page

---
+++ lib/component/CustomTextInput.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_svg/flutter_svg.dart';
+import '../Components/CustomTextInput.dart';
+
+class SignInForm extends StatelessWidget {
+  const SignInForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          CustomTextInput(
+            hintText: "Enter your email",
+            labelText: "Email",
+            suffixIcon: SvgPicture.string(mailIcon),
+            textInputAction: TextInputAction.next,
+            onChanged: (email) {},
+            onSaved: (email) {},
+          ),
+          const SizedBox(height: 24),
+          CustomTextInput(
+            hintText: "Enter your password",
+            labelText: "Password",
+            suffixIcon: SvgPicture.string(lockIcon),
+            obscureText: true,
+            textInputAction: TextInputAction.done,
+            onChanged: (password) {},
+            onSaved: (password) {},
+          ),
+          const SizedBox(height: 8),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              backgroundColor: const Color(0xFFFF7643),
+              foregroundColor: Colors.white,
+              minimumSize: const Size(double.infinity, 48),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+              ),
+            ),
+            child: const Text("Continue"),
+          ),
+        ],
+      ),
+    );
+  }
+}
