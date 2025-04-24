aee838003e3bc48d6c2f45031470a99a66fd5aee
mataa019 <johnnymataa019@gmail.com>
2025-04-24 12:11:20 +0200

    Setup Signup page

--- lib/Pages/ForgotPasswordScreen.dart
+++ lib/Pages/ForgotPasswordScreen.dart
@@ -9,138 +9,107 @@ class ChangePasswordScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: LogoWithTitle(
-        title: "Change Password",
-        children: [
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                CustomTextInput(
-                  hintText: "Enter your new password",
-                  labelText: "New Password",
-                  obscureText: true,
-                  suffixIcon: const Icon(Icons.lock, color: Color(0xFF757575)),
-                  onChanged: (password) {
-                    // Handle password input change
-                  },
-                  onSaved: (password) {
-                    // Save password
-                  },
-                ),
-                const SizedBox(height: 16),
-                CustomTextInput(
-                  hintText: "Confirm your new password",
-                  labelText: "Confirm Password",
-                  obscureText: true,
-                  suffixIcon: const Icon(Icons.lock_outline, color: Color(0xFF757575)),
-                  onChanged: (password) {
-                    // Handle confirm password input change
-                  },
-                  onSaved: (password) {
-                    // Save confirm password
-                  },
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 24),
-          ElevatedButton(
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                _formKey.currentState!.save();
-              }
-            },
-            style: ElevatedButton.styleFrom(
-              elevation: 0,
-              backgroundColor: const Color(0xFFFF7643),
-              foregroundColor: Colors.white,
-              minimumSize: const Size(double.infinity, 48),
-              shape: const StadiumBorder(),
-            ),
-            child: const Text("Change Password"),
-          ),
-          const SizedBox(height: 16),
-          TextButton(
-            onPressed: () {
-              // Handle navigation to Sign In screen
-              Navigator.pop(context);
-            },
-            child: Text.rich(
-              TextSpan(
-                text: "Already have an account? ",
-                children: [
-                  TextSpan(
-                    text: "Sign in",
-                    style: TextStyle(color: Theme.of(context).primaryColor),
-                  ),
-                ],
-              ),
-              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: Theme.of(context)
-                        .textTheme
-                        .bodyLarge!
-                        .color!
-                        .withOpacity(0.64),
-                  ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class LogoWithTitle extends StatelessWidget {
-  final String title, subText;
-  final List<Widget> children;
-
-  const LogoWithTitle({
-    super.key,
-    required this.title,
-    this.subText = '',
-    required this.children,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: LayoutBuilder(builder: (context, constraints) {
-        return SingleChildScrollView(
+      body: SafeArea(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Column(
             children: [
-              SizedBox(
-                height: constraints.maxHeight * 0.1,
-                width: double.infinity,
+              const SizedBox(height: 40),
+              // Inline logo
+              Image.asset(
+                "assets/images/logo.png", // Replace with your logo path
+                height: 100,
               ),
+              const SizedBox(height: 16),
+              // Title
               Text(
-                title,
+                "Change Password",
                 style: Theme.of(context)
                     .textTheme
                     .headlineSmall!
                     .copyWith(fontWeight: FontWeight.bold),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Text(
-                  subText,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    height: 1.5,
-                    color: Theme.of(context)
-                        .textTheme
-                        .bodyLarge!
-                        .color!
-                        .withOpacity(0.64),
-                  ),
+              const SizedBox(height: 16),
+              // Form
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    CustomTextInput(
+                      hintText: "Enter your new password",
+                      labelText: "New Password",
+                      obscureText: true,
+                      suffixIcon: const Icon(Icons.lock, color: Color(0xFF757575)),
+                      onChanged: (password) {
+                        // Handle password input change
+                      },
+                      onSaved: (password) {
+                        // Save password
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    CustomTextInput(
+                      hintText: "Confirm your new password",
+                      labelText: "Confirm Password",
+                      obscureText: true,
+                      suffixIcon: const Icon(Icons.lock_outline, color: Color(0xFF757575)),
+                      onChanged: (password) {
+                        // Handle confirm password input change
+                      },
+                      onSaved: (password) {
+                        // Save confirm password
+                      },
+                    ),
+                  ],
                 ),
               ),
-              ...children,
+              const SizedBox(height: 24),
+              // Change Password Button
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: const Color(0xFFFF7643),
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 48),
+                  shape: const StadiumBorder(),
+                ),
+                child: const Text("Change Password"),
+              ),
+              const SizedBox(height: 16),
+              // Already have an account? Sign in
+              TextButton(
+                onPressed: () {
+                  // Handle navigation to Sign In screen
+                  Navigator.pop(context);
+                },
+                child: Text.rich(
+                  TextSpan(
+                    text: "Already have an account? ",
+                    children: [
+                      TextSpan(
+                        text: "Sign in",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      ),
+                    ],
+                  ),
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .color!
+                            .withOpacity(0.64),
+                      ),
+                ),
+              ),
             ],
           ),
-        );
-      }),
+        ),
+      ),
     );
   }
 }
