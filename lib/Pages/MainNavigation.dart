546d7e8a6c8ce301cc6f22129febabf796858d84
mataa019 <johnnymataa019@gmail.com>
2025-04-24 13:37:10 +0200

    Setup Signup page

---
+++ lib/Pages/MainNavigation.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+import 'CartPage.dart';
+import 'BookedClassesPage.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  // List of pages for navigation
+  final List<Widget> _pages = [
+    const HomePage(),
+    const CartPage(),
+    const BookedClassesPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex], // Display the selected page
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: "Booked",
+          ),
+        ],
+      ),
+    );
+  }
+}
