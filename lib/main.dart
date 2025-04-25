057cde08d368e31d15e954cf7d7c3118173990f6
mataa019 <johnnymataa019@gmail.com>
2025-04-25 10:32:06 +0200

    set up firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Pages/MainNavigation.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'Pages/splash.dart';
 import 'Auth/Login.dart';
 
