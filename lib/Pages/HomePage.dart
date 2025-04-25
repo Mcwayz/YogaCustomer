21ecb4bcddf875382b295a021d1f2919392cfeb5
mataa019 <johnnymataa019@gmail.com>
2025-04-25 11:03:40 +0200

    set up firebase Auth

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -77,8 +77,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Yoga Classes"),
+      appBar: CustomAppBar(
+        title: "Yoga Classes",
+        centerTitle: false
       ),
       body: Column(
         children: [
